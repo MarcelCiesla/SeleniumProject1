9913f70f1dd1fce22f954f572c036f8ea8e81011
Stiku2137 <mciesla97@gmail.com>
2022-10-31 15:06:15 +0100

    Set parents button, child button, Search button, hotel list and assert

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -2,9 +2,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
@@ -20,6 +23,30 @@ public class HotelSearch {
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
         driver.findElement(By.name("checkin")).sendKeys("02/11/2022");
         driver.findElement(By.name("checkout")).sendKeys("05/11/2022");
+
+        //korzystanie z kalendarza
+        driver.findElement(By.name("checkin")).findElement(By.xpath("//td[@class='day ' and text()='30']")).click();
+        driver.findElement(By.name("checkout")).findElement(By.xpath("//td[@class='day ' and text()='30']")).click();
+
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
+        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b")).stream()
+                                                                                            .map(el -> el.getAttribute("textContent"))
+                                                                                            .collect(Collectors.toList());
+        /*System.out.println(hotelNames.size());
+        hotelNames.forEach(el -> System.out.println(el));*/
+
+        hotelNames.forEach(System.out::println);
+
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
+
 
     }
 }
