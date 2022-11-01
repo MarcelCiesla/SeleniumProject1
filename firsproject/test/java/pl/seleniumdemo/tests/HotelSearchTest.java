14197b6755b5852792ca543345f7fbb069c29c8e
Stiku2137 <mciesla97@gmail.com>
2022-11-01 20:44:03 +0100

    page object list

--- firsproject/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ firsproject/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.seleniumdemo.pages.HotelSearchPage;
+import pl.seleniumdemo.pages.ResultsPage;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +20,10 @@ public class HotelSearchTest extends BaseTest {
         hotelSearchPage.setTravellers();
         hotelSearchPage.performSearch();
 
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
+
+        // Tu przyklad bez PageObjectu
 
        /* driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys("Dubai");
@@ -35,14 +40,14 @@ public class HotelSearchTest extends BaseTest {
         driver.findElement(By.id("childPlusBtn")).click();
 
         driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
 */
-        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b")).stream()
+       /* List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b")).stream()
                                                                                             .map(el -> el.getAttribute("textContent"))
                                                                                             .collect(Collectors.toList());
         System.out.println(hotelNames.size());
         hotelNames.forEach(el -> System.out.println(el));
-
-        hotelNames.forEach(System.out::println);
+        hotelNames.forEach(System.out::println);*/
 
         Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
         Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
